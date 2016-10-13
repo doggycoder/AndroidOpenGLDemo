1f28ce485c63d9901baf05679129ec4eac1537a2
majun@aiyaapp.com <4039807faa6f>
2016-10-13 19:23:15 +0800

    ic

--- app/src/main/java/edu/wuwang/opengl/FGLViewActivity.java
+++ app/src/main/java/edu/wuwang/opengl/FGLViewActivity.java
@@ -10,15 +10,28 @@ package edu.wuwang.opengl;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Button;
+
+import java.util.HashMap;
 
 /**
  * Description:
  */
 public class FGLViewActivity extends Activity {
 
+    private Button mChange;
+    private FGLView mGLView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new FGLView(this));
+        setContentView(R.layout.activity_fglview);
+        init();
     }
+
+    private void init(){
+        mChange= (Button) findViewById(R.id.mChange);
+        mGLView= (FGLView) findViewById(R.id.mGLView);
+    }
+
 }
