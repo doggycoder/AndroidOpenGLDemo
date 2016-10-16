e7d79a37119573d3a81b04066b53a3ed54277b84
doggycoder <1558183202@qq.com>
2016-10-17 00:34:58 +0800

    散射光照球

--- app/src/main/assets/fshader/BallWithLight.sh
+++ app/src/main/assets/fshader/BallWithLight.sh
@@ -3,5 +3,5 @@ varying vec4 vDiffuse;//接收从顶点着色器过来的散射光分量
 void main(){
    vec4 finalColor=vec4(1.0);
    //给此片元颜色值
-   gl_FragColor=finalColor*vDiffuse+finalColor*vec4(0.3,0.3,0.3,1.0);
+   gl_FragColor=finalColor*vDiffuse+finalColor*vec4(0.15,0.15,0.15,1.0);
 }
