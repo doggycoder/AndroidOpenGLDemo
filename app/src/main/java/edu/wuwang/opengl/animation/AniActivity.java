ebf7ca6185aa4b4f9d7367a7033fa2cdd25f9dff
majun@aiyaapp.com <4039807faa6f>
2016-11-29 19:36:02 +0800

    简化预览

---
+++ app/src/main/java/edu/wuwang/opengl/animation/AniActivity.java
@@ -0,0 +1,25 @@
+/*
+ *
+ * AniActivity.java
+ * 
+ * Created by Wuwang on 2016/11/29
+ * Copyright © 2016年 深圳哎吖科技. All rights reserved.
+ */
+package edu.wuwang.opengl.animation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description:
+ */
+public class AniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
---
+++ app/src/main/java/edu/wuwang/opengl/animation/MultiImgDrawer.java
@@ -0,0 +1,33 @@
+/*
+ *
+ * MultiImgDrawer.java
+ * 
+ * Created by Wuwang on 2016/11/29
+ * Copyright © 2016年 深圳哎吖科技. All rights reserved.
+ */
+package edu.wuwang.opengl.animation;
+
+import android.content.res.Resources;
+
+import edu.wuwang.opengl.filter.AFilter;
+
+/**
+ * Description:
+ */
+public class MultiImgDrawer extends AFilter {
+
+
+    public MultiImgDrawer(Resources mRes) {
+        super(mRes);
+    }
+
+    @Override
+    protected void onCreate() {
+
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height) {
+
+    }
+}
