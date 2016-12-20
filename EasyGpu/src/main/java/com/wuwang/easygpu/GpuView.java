04e9ceadeab05a52ccb6a35780a4e3a750c11b45
doggycoder <1558183202@qq.com>
2016-12-20 23:09:03 +0800

    msg

---
+++ EasyGpu/src/main/java/com/wuwang/easygpu/GpuView.java
@@ -0,0 +1,22 @@
+package com.wuwang.easygpu;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * Created by wuwang on 2016/12/20
+ */
+
+public class GpuView extends GLSurfaceView {
+
+
+    public GpuView(Context context) {
+        super(context);
+    }
+
+    public GpuView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
---
+++ EasyGpu/src/main/java/com/wuwang/easygpu/core/OesFilter.java
@@ -0,0 +1,8 @@
+package com.wuwang.easygpu.core;
+
+/**
+ * Created by wuwang on 2016/12/20
+ */
+
+public class OesFilter {
+}
